4ccea3a5d81f1a4f6f2b3e1da955a7196b7d7c62
Zachary Fein <zfein@caltech.edu>
2015-04-28 02:51:20 -0700

    added in baseline averages to sgd

--- src/LoadData.h
+++ src/LoadData.h
@@ -27,13 +27,18 @@ class LoadData
         //virtual ~KNN();
         //arma::umat getKNN();
         double getUserStddev(int userIdx);
+        double getGlobalMean();
         double getMovieMean(int movieIdx);
         double getMovieStddev(int movieIdx);
+        double getBetterUserMean(int userIdx);
+        double getBetterMovieMean(int movieIdx);
 
     protected:
     private:
         unordered_map<int, vector<double>> userMap;
         unordered_map<int, vector<double>> movieMap;
+        int totalMovies;
+        int sumRatings;
         arma::umat data;
 };
 
