e97cb6024aebf34b6492c8079cd192ace56b66c3
Elaine Arbaugh <earbaugh@caltech.edu>
2015-05-20 13:59:28 -0700

    Quiz blend stuff

--- src/QuizBlend.cpp
+++ src/QuizBlend.cpp
@@ -2,8 +2,8 @@
 
 QuizBlend::QuizBlend()
 {
-    outfile = "src/519quizblend.dta";
-    outcoef = "src/519coefs.dta";
+    outfile = "5192quizblend.dta";
+    outcoef = "5192coefs.dta";
     variance = 1.274;
     mean = 3.674;
     qual_size = 2749898;
@@ -15,13 +15,44 @@ QuizBlend::QuizBlend()
 
     // Open a buncha files and add to the matrix
     std::vector<std::string> files;// = ["sgd_results26.txt", "SGDpp_results19.txt"];
-    files.push_back("src/sgd_results26.txt");
-    files.push_back("src/SGDpp_results19.txt");
+    files.push_back("sgd_results26.txt");
+    files.push_back("SGDpp_results19.txt");
+    files.push_back("SGDpp_results17.txt");
+    files.push_back("../../552sgd_results.dta");
+    files.push_back("../../sgd_results24.txt");
+    files.push_back("../../SGDppelaine_results10.txt");
+    files.push_back("../../SGDpp_results13.txt");
+    files.push_back("../../sgd_results21.txt");
+    files.push_back("../../56best_sgd_results.dta");
+    files.push_back("../../SGDppelaine_results9.txt");
+    files.push_back("../../SGDpp_results11.txt");
+    files.push_back("../../SGDpp_results12.txt");
+    files.push_back("../../SGDpp_results15.txt");
+    files.push_back("../../SGDpp_results16.txt");
+    files.push_back("../../sgd_results22.txt");
+    files.push_back("../../sgd_results23.txt");
+    files.push_back("../../sgd_results18.txt");
     num_files = files.size();
     ratings = arma::mat(qual_size, num_files);
     //rmse = [.90399, .90242];
     rmse.push_back(.90399);
     rmse.push_back(.902424);
+    rmse.push_back(.90316);
+    rmse.push_back(.90536);
+    rmse.push_back(.90465);
+    rmse.push_back(.90323);
+    rmse.push_back(.90369);
+    rmse.push_back(.90541);
+    rmse.push_back(.90658);
+    rmse.push_back(.90472);
+    rmse.push_back(.90589);
+    rmse.push_back(.90552);
+    rmse.push_back(.90322);
+    rmse.push_back(.90319);
+    rmse.push_back(.90571);
+    rmse.push_back(.90666);
+    rmse.push_back(.90638);
+
     std::cout << "Creating matrix" << std::endl;
     for (int i = 0; i < num_files; ++i) {
         std::ifstream f0;
@@ -93,9 +124,11 @@ void QuizBlend::run_blend()
     outf.close();
 }
 
+/*
 int main()
 {
     QuizBlend q = QuizBlend();
     q.run_blend();
     return 0;
 }
+*/
